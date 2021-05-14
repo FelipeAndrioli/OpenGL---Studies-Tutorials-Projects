ea643c37dfa8fd47647d75a416140751d4b88ee2
Felipe Andrioli <felipeandrioli56@hotmail.com>
2021-05-14 16:42:14 -0300

    Added triangle fan

---
+++ tutorial/triangle_fan/main.cpp
@@ -0,0 +1,56 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <cstdlib>
+
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+
+int main(int argc, char** argv) {
+    GLFWwindow* window;
+
+    if (!glfwInit()) {
+        std::cout << "The application have failed initializing the library" << std::endl;
+        return -1;
+    }
+
+    window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Triangle Fan", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    glViewport(0.0f, 0.0f, SCREEN_WIDTH, SCREEN_HEIGHT);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0, SCREEN_WIDTH, 0, SCREEN_HEIGHT, 0, 1);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    GLfloat polygonVertices[] = {
+        320, 240, 0,
+        370, 290, 0,
+        420, 240, 0,
+        370, 190, 0
+    };
+
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glEnableClientState(GL_VERTEX_ARRAY);
+        glVertexPointer(3, GL_FLOAT, 0, polygonVertices);
+        glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
+        glDisableClientState(GL_VERTEX_ARRAY);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    
+    return 0;
+}
