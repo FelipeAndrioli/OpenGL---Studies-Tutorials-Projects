f00cf3c0e84d6d2a2d260cc9f7a6b8b91607a4e6
Felipe Andrioli <felipeandrioli56@hotmail.com>
2021-05-14 13:28:57 -0300

    Added draw quad

---
+++ tutorial/quad/main.cpp
@@ -0,0 +1,48 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <cstdlib>
+
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+
+int main(int argc, char** argv) {
+    GLFWwindow* window;
+
+    if (!glfwInit()) {
+        std::cout << "The application failed initializing the library" << std::endl;
+        return -1;
+    }
+
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    GLfloat vertices[] = {
+        0.5, 0.5, 0.0,  //top right corner
+        -0.5, 0.5, 0.0,  //top leftr corner
+        -0.5, -0.5, 0.0,  //bottom left corner
+        0.5, -0.5, 0.0   //bottom right corner
+    };
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glEnableClientState(GL_VERTEX_ARRAY);
+        glVertexPointer(3, GL_FLOAT, 0, vertices);
+        glDrawArrays(GL_QUADS, 0, 4);
+        glDisableClientState(GL_VERTEX_ARRAY);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
+    return 0;
+}
