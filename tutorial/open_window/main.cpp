829cc868b0670bbe7d44059cc3a9ecb0111e94fa
Felipe Andrioli <felipeandrioli56@hotmail.com>
2021-05-14 14:29:55 -0300

    Added additional configurations and comments to explain

--- tutorial/open_window/main.cpp
+++ tutorial/open_window/main.cpp
@@ -25,6 +25,39 @@ int main(int argc, char** argv) {
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    /*
+        Specifies the part of the window to which OpenGL will draw (in pixels), convert from normalised to pixels
+    */
+    glViewport(0.0f, 0.0f, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+    /*
+        Projection matrix defines the properties of the camera that views the objects in the world coordinate frame. 
+        Here you typically set the zoom factor, aspect ration and the near and far clipping planes
+    */
+    glMatrixMode(GL_PROJECTION);
+
+    /*
+        Replace the current matrix with the identity matrix and starts us a fresh because matrix transforms such as 
+        glOrpho and glRotate cumulate, basically puts us at (0, 0, 0)
+    */
+    glLoadIdentity();
+
+    /*
+        Essentially set coordinate system
+    */
+    glOrtho(0, SCREEN_WIDTH, 0, SCREEN_HEIGHT, 0, 1);
+
+    /*
+        (Default matrix mode) Modelview matrix defines how your objects are transformed (meaning translation, rotation 
+        and scaling) in your world
+    */
+    glMatrixMode(GL_MODELVIEW);
+
+    /*
+        Same as the above
+    */
+    glLoadIdentity();
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
