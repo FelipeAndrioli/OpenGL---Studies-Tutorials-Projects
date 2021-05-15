70d29f79e701be1293df3270d777066f56f556ed
Felipe Andrioli <felipeandrioli56@hotmail.com>
2021-05-15 01:17:16 -0300

    Added quad strip

---
+++ tutorial/quad_strip/main.cpp
@@ -0,0 +1,60 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <cstdlib>
+
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+
+int main(int argc, char** argv) {
+    GLFWwindow* window;
+
+    if (!glfwInit()) {
+        std::cout << "The application have failed initializing the library" << std::endl;
+        return -1;
+    }
+
+    window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Hello World", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    glViewport(0.0f, 0.0f, SCREEN_WIDTH, SCREEN_HEIGHT);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0, SCREEN_WIDTH, 0, SCREEN_HEIGHT, 0, 1);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    GLfloat vertices[] = {
+        200, 300, 0.0,  //top left corner
+        300, 300, 0.0,  //top right corner
+        200, 250, 0.0,  //bottom left corner
+        300, 250, 0.0,  //bottom right corner
+
+        320, 150, 0.0,
+        350, 200, 0.0,
+
+        400, 50, 0.0,
+        450, 120, 0.0
+    };
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glEnableClientState(GL_VERTEX_ARRAY);
+        glVertexPointer(3, GL_FLOAT, 0, vertices);
+        glDrawArrays(GL_QUAD_STRIP, 0, 8);
+        glDisableClientState(GL_VERTEX_ARRAY);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    
+    return 0;
+}
