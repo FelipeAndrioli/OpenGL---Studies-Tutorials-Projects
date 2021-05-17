5c44d535c6250a93a06dacaf4b5a6d1adb585516
Felipe Andrioli <felipeandrioli56@hotmail.com>
2021-05-17 11:03:02 -0300

    Added keyboard input sample

---
+++ tutorial/keyboard_input/main.cpp
@@ -0,0 +1,61 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <cstdlib>
+
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+
+void keyCallback(GLFWwindow *window, int key, int scancode, int action, int mods) {
+
+    std::cout << key << std::endl;
+
+    //actions are GLFW_PRESS, GLFW_RELEASE or GLFW_REPEAT
+    if (key == GLFW_KEY_SPACE && action == GLFW_REPEAT) {
+        std::cout << "Space key pressed" << std::endl;
+    }
+}
+
+int main(int argc, char** argv) {
+    GLFWwindow* window;
+
+    if (!glfwInit()) {
+        std::cout << "The application have failed initializing the library" << std::endl;
+        return -1;
+    }
+
+    window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Hello World", NULL, NULL);
+
+    int screenWidth;
+    int screenHeight;
+
+    glfwGetFramebufferSize(window, &screenWidth, &screenHeight);
+
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    glfwSetKeyCallback(window, keyCallback);
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, 1);
+
+    glViewport(0.0f, 0.0f, screenWidth, screenHeight);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0, SCREEN_WIDTH, 0, SCREEN_HEIGHT, 0, 1);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    
+    return 0;
+}
