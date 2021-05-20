e47d1141cffa3b10f8120e399b7366ca92cd09da
Felipe Andrioli <felipeandrioli56@hotmail.com>
2021-05-20 11:47:32 -0300

    Split main.cpp into Window.h, Mouse.h and Keyboard.h

--- opengl-gameprogramming/base_project/main.cpp
+++ opengl-gameprogramming/base_project/main.cpp
@@ -5,167 +5,45 @@
 #include <cstring>
 #include <array>
 
-#define SCREEN_WIDTH 640
-#define SCREEN_HEIGHT 480
+#include "Window.h"
 
-class Window {
-    private:
-        std::array<int, 2> windowPosition = {0, 0};
-        std::array<int, 2> windowSize = {0, 0};
-        std::array<int, 2> viewPortSize {0, 0};
-        bool updateViewport = true;
-        GLFWwindow* window = NULL;
-        GLFWmonitor* monitor = NULL;
-
-        void Resize(int cx, int cy);
-
-    public:
-
-        void Init(int width, int height);
-        static void CallbackResize(GLFWwindow* window, int cx, int cy);
-        void MainLoop();
-        bool IsFullScreen();
-        void SetFullScreen(bool fullscreen);
-};
-
-void Window::Init(int width, int height) {
-    window = glfwCreateWindow(width, height, "Window.exe", NULL, NULL);
-
-    if(window == NULL) {
-        glfwTerminate();
-        throw std::runtime_error("Error initializing window");
-    }
-
-    glfwMakeContextCurrent(window);
-
-    glfwSetWindowUserPointer(window, NULL);
-    glfwSetWindowSizeCallback(window, Window::CallbackResize);
-
-    monitor = glfwGetPrimaryMonitor();
-    glfwGetWindowSize(window, &windowSize[0], &windowSize[1]);
-    glfwGetWindowPos(window, &windowPosition[0], &windowPosition[1]);
-
-    updateViewport = true;
-}
-
-void Window::CallbackResize(GLFWwindow* window, int cx, int cy) {
-    void *windowUserPointer = glfwGetWindowUserPointer(window);
-
-    if(Window *windowPointer = static_cast<Window*>(windowUserPointer)) {
-        windowPointer -> Resize(cx, cy);
-    }
-}
-
-void Window::Resize(int cx, int cy) {
-    updateViewport = true;
-}
-
-void Window::MainLoop() {
-    while (!glfwWindowShouldClose(window)) {
-        if (updateViewport) {
-            glfwGetFramebufferSize(window, &viewPortSize[0], &viewPortSize[1]);
-            glViewport(0, 0, viewPortSize[0], viewPortSize[1]);
-            updateViewport = false;
-        }
-
-        //render here
-
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-}
-
-bool Window::IsFullScreen() {
-    return glfwGetWindowMonitor(window) != NULL;
-}
-
-void Window::SetFullScreen(bool fullscreen) {
-    if (IsFullScreen() == fullscreen) {
-        return;
-    }
-
-    if (fullscreen) {
-        //backup window position and window size
-        glfwGetWindowPos(window, &windowPosition[0], &windowPosition[0]);
-        glfwGetWindowSize(window, &windowSize[0], &windowSize[1]);
-
-        //get resolution of monitor
-        const GLFWvidmode *mode = glfwGetVideoMode(glfwGetPrimaryMonitor());
-
-        //switch to full screen
-        glfwSetWindowMonitor(window, monitor, 0, 0, mode -> width, mode -> height, 0);
-    } else {
-        //restore last window size and position
-        glfwSetWindowMonitor(window, NULL, windowPosition[0], windowPosition[1], windowSize[0], windowSize[1], 0);
-    }
-
-    updateViewport = true;
-}
-
-void keyCallback (GLFWwindow *window, int key, int scancode, int action, int mods) {
-    if (action == GLFW_PRESS) {
-        switch (key) {
-            case GLFW_KEY_UP:
-                std::cout << "Up arrow pressed" << std::endl;
-                break;
-            case GLFW_KEY_DOWN:
-                std::cout << "Down arrow pressed" << std::endl;
-                break;
-            case GLFW_KEY_LEFT:
-                std::cout << "Left arrow pressed" << std::endl;
-                break;
-            case GLFW_KEY_RIGHT:
-                std::cout << "Right arrow pressed" << std::endl;
-                break;
-        }
-    }
-}
-
-static void cursorPositionCallback(GLFWwindow *window, double xpos, double ypos) {
-    std::cout << xpos << " : " << ypos << std::endl;
-}
-
-void cursorEnterCallback(GLFWwindow *window, int entered) {
-    if (entered) {
-        std::cout << "Entered Window" << std::endl;
-    }
-}
-
-void mouseButtonCallback(GLFWwindow *window, int button, int action, int mods) {
-    double ypos;
-    double xpos;
-
-    if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS) {
-        std::cout << "Right button pressed" << std::endl;
-        glfwGetCursorPos(window, &xpos, &ypos);
-        std::cout << xpos << " : " << ypos << std::endl;
-    } else if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {
-        std::cout << "Left button pressed" << std::endl;
-        glfwGetCursorPos(window, &xpos, &ypos);
-        std::cout << xpos << " : " << ypos << std::endl;
-    }
-}
-
-void scrollCallback(GLFWwindow *window, double xoffset, double yoffset) {
-    std::cout << xoffset << " : " << yoffset << std::endl;
-}
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
 
 int main(int argc, char** argv) {
-    GLFWwindow* window;
+    //GLFWwindow* window;
 
     if (!glfwInit()) {
         std::cout << "The application have failed initializing the library" << std::endl;
         return -1;
     }
 
+    Window window;
+    window.Init(SCREEN_WIDTH, SCREEN_HEIGHT);
+    //window.SetFullScreen(true);
+    window.MainLoop();
+
+/*
+    GLFWmonitor* monitor = glfwGetPrimaryMonitor();
+    const GLFWvidmode* mode = glfwGetVideoMode(monitor);
+
+    glfwWindowHint(GLFW_RED_BITS, mode -> redBits);
+    glfwWindowHint(GLFW_GREEN_BITS, mode -> greenBits);
+    glfwWindowHint(GLFW_BLUE_BITS, mode -> blueBits);
+    glfwWindowHint(GLFW_REFRESH_RATE, mode -> refreshRate);
+
+    GLFWwindow* window = glfwCreateWindow(mode -> width, mode -> height, "Main.exe", monitor, NULL);
+*/
+/*
     GLFWmonitor* monitor = glfwGetPrimaryMonitor();
 
     window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Base Project.exe", monitor, NULL);
 
     glfwSetKeyCallback(window, keyCallback);
     glfwSetInputMode(window, GLFW_STICKY_KEYS, 1);
-
+*/
     //glfwSetCursorPosCallback(window, cursorPositionCallback);
+/*
     glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
     glfwSetCursorEnterCallback(window, cursorEnterCallback);
     glfwSetMouseButtonCallback(window, mouseButtonCallback);
@@ -180,7 +58,8 @@ int main(int argc, char** argv) {
     image.pixels = pixels;
     GLFWcursor *cursor = glfwCreateCursor(&image, 0, 0);
     glfwSetCursor(window, cursor);
-
+*/
+/*
     int screenWidth;
     int screenHeight;
 
@@ -224,6 +103,6 @@ int main(int argc, char** argv) {
     }
 
     glfwTerminate();
-    
+*/
     return 0;
 }
