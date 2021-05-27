4d1132b61d13fa0c061d10ea6575216f66793671
Felipe Andrioli <felipeandrioli56@hotmail.com>
2021-05-27 11:06:47 -0300

    Added chapter 3 on your own directory

---
+++ opengl-gameprogramming/chapter_3/on_your_own/main.cpp
@@ -0,0 +1,26 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <vector>
+
+#include "headers/Window.h"
+
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
+
+int main(int argc, char** argv) {
+
+    if (!glfwInit()) {
+        std::cout << "The application have failed initializing the library" << std::endl;
+        return -1;
+    }
+
+    Window window;
+    window.Init(SCREEN_WIDTH, SCREEN_HEIGHT);
+    //window.SetFullScreen(true);
+    window.MainLoop();
+
+    return 0;
+}
