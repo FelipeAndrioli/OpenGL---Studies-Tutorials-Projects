fc247dc8649f11452aade95d44b21f127c6a844f
Felipe Andrioli <felipeandrioli56@hotmail.com>
2021-06-02 10:12:46 -0300

    Changing the draw from triangle fan to a square, drawed by a triangle strip

--- opengl-gameprogramming/chapter_4/rotation_example/headers/Draw.h
+++ opengl-gameprogramming/chapter_4/rotation_example/headers/Draw.h
@@ -7,11 +7,11 @@
 */
 
 void Draw(GLint screen_width, GLint screen_height) {
-    //glMatrixMode(GL_PROJECTION);
-    //glLoadIdentity();
-    //glOrtho(0, screen_width, 0, screen_height, 0, 1);
-    //glMatrixMode(GL_MODELVIEW);
-    //glLoadIdentity();
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0, screen_width, 0, screen_height, 0, 1);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
 
     GLfloat polygonVertices[] = {
         276.0, 380.0, 0.0,
@@ -21,7 +21,6 @@ void Draw(GLint screen_width, GLint screen_height) {
     };
 
     glClear(GL_COLOR_BUFFER_BIT);
-    glTranslatef(5.0f, 5.0f, 5.0f);
     glEnableClientState(GL_VERTEX_ARRAY);
     glVertexPointer(3, GL_FLOAT, 0, polygonVertices);
     glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
