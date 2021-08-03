e911774220f48c81beafa8204c78ca2c0802e013
Felipe Andrioli <felipeandrioli56@hotmail.com>
2021-08-02 21:41:06 -0300

    Make window works in the new architecture

--- opengl-gameprogramming/base_project/main.cpp
+++ opengl-gameprogramming/base_project/main.cpp
@@ -1,4 +1,8 @@
-#include "headers/Window.h"
+#include <iostream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include "Libraries/Output Library/Window.h"
 
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 600
@@ -12,6 +16,11 @@ int main(int argc, char** argv) {
 
     Window window;
     window.init("Main.exe", SCREEN_WIDTH, SCREEN_HEIGHT, false);
+
+    while(!glfwWindowShouldClose(window.getWindow())) {
+        glfwSwapBuffers(window.getWindow());
+        glfwPollEvents();
+    }
 
     return 0;
 }
