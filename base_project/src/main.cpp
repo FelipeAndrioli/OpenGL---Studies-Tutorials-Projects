8ce3b32f29d5ed90e6a063511b09b5989e5ddb8b
Felipe Andrioli <felipeandrioli56@hotmail.com>
2021-12-29 00:06:56 -0300

    Moving main file to src directory

---
+++ base_project/src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
+
+int main(int argc, char** argv) {
+
+
+
+    return 0;
+}
