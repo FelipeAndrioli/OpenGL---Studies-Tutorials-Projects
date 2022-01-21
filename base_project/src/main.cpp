70c1d1f8ae0b1f09dec8594452bc1375c6507196
Felipe Andrioli <felipeandrioli56@hotmail.com>
2022-01-21 12:06:08 -0300

    Added small piece of code to be executed

--- base_project/src/main.cpp
+++ base_project/src/main.cpp
@@ -5,9 +5,11 @@
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 600
 
+using namespace std;
+
 int main(int argc, char** argv) {
 
-
+    cout << "Testing compilation!" << endl;
 
     return 0;
 }
