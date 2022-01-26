b5143cb80daa8a2f80a441099dd9f16ec9f6604b
Felipe Andrioli <felipeandrioli56@hotmail.com>
2022-01-26 16:53:05 -0300

    Added comments about VBO, VAO and EBO. Added a function to switch between polygon modes when pressing 1 and 2 in the keyboard as well

--- learnopengl/hello_triangle/src/main.cpp
+++ learnopengl/hello_triangle/src/main.cpp
@@ -25,6 +25,14 @@ void processInput(GLFWwindow *window) {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
         glfwSetWindowShouldClose(window, true);
     }
+
+    if (glfwGetKey(window, GLFW_KEY_1) == GLFW_PRESS) {
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    }
+    
+    if (glfwGetKey(window, GLFW_KEY_2) == GLFW_PRESS) {
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    }
 }
 
 int main(int argc, char** argv) {
@@ -114,6 +122,42 @@ int main(int argc, char** argv) {
         1, 2, 3     // second triangle
     };
 
+    /*
+        Vertex Buffer Object(VBO) -> When the vertex data is defined, we'd like to send it as
+        input to the first process of the graphics pipeline: the vertex shader. This is done by
+        creating memory on the GPU where we store the vertex data, configure how OpenGL should
+        interpret the memory and specify how to send the data to the graphics card. The vertex
+        shader then processes as much vertices as we tell it to from its memory.
+
+        We manage this memory via so called vertex buffer objects (VBO) that can store a large
+        number of vertices in the GPU's memory. The advantage of using those buffer objects is
+        that we can send large batches of data all at once to the graphics card, and keep it there
+        if there's enough memory left, without having to send data one vertex at a time. Sending
+        data to the graphics card from the CPU is relatively slow, so wherever we can we try to
+        send as much data as possible at once. Once the data is in the graphics card's memory the
+        vertex shader has almost instant access to the vertices making it extremely fast.
+
+        Vertex Array Object(VAO) -> A vertex array object can be bound just like a vertex buffer
+        object and any subsequent vertex attribute calls from that point on will be stored inside
+        the VAO. This has the advantage that when configuring vertex attribute pointers you only
+        have to make those calls once and whenever we want to draw the object, we can just bind
+        the corresponding VAO. This makes switching between different vertex data and attribute
+        configuration as easy as binding a different VAO. All the state we just set is stored 
+        inside the VAO.
+
+        A Vertex Array Object is an OpenGL Object that stores all of the state needed to supply
+        vertex data. It stores the format of the vertex data as well as the Buffer Objects providing
+        the vertex data arrays. Note that VAOs do not copy, freeze or store the contents of the
+        referenced buffers - if you change any of the data in the buffers referenced by an existing
+        VAO, those changes will be seen by users of the VAO.
+
+        Element Buffer Object(EBO) -> An EBO is a buffer, just like a vertex buffer object, that
+        stores indices that OpenGL uses to decide what vertices to draw. This is so called indexed
+        drawing. Similar to the VBO we bind the EBO and copy the indices into the buffer with 
+        glBufferData. Also, just like the VBO we want to place those calls between a bind and an
+        unbind call, although this time we specify GL_ELEMENT_ARRAY_BUFFER as the buffer type.
+    */
+
     unsigned int EBO;
     unsigned int VBO;
     unsigned int VAO;
@@ -145,7 +189,7 @@ int main(int argc, char** argv) {
     glBindVertexArray(0);
 
     // uncomment this call to draw in wireframe polygons
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
     // render loop
     while (!glfwWindowShouldClose(window)) {
