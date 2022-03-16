e95aaeb882c2b400205a163f15efcfd0df08cb2e
Felipe Andrioli <felipeandrioli56@hotmail.com>
2022-03-15 21:11:24 -0300

    Deleting shaders after link shader program

--- learnopengl/playground/src/main.cpp
+++ learnopengl/playground/src/main.cpp
@@ -95,6 +95,9 @@ int main() {
         std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
     }
 
+   glDeleteShader(vertexShader);
+   glDeleteShader(fragmentShader); 
+
     while (!glfwWindowShouldClose(window)) {
         processInput(window);
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
