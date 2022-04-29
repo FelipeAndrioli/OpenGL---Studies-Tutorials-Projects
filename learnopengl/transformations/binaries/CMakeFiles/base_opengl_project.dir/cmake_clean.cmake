95fb62597046dab7123f7ab0bc66bcc7ea78299a
Felipe Andrioli <felipeandrioli56@hotmail.com>
2022-04-29 09:03:58 -0300

    Adding dependencies

---
+++ learnopengl/transformations/binaries/CMakeFiles/base_opengl_project.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/base_opengl_project.dir/src/main.cpp.obj"
+  "CMakeFiles/base_opengl_project.dir/src/main.cpp.obj.d"
+  "libmain.dll.a"
+  "main.exe"
+  "main.exe.manifest"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/base_opengl_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
