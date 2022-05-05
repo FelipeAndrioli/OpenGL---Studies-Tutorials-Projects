ef6405655455ab5b1131af42b178906118f23442
Felipe Andrioli <felipeandrioli56@hotmail.com>
2022-05-05 20:26:03 -0300

    Added support to two textures in the same object

--- learnopengl/transformations/src/shaders/shader.fs
+++ learnopengl/transformations/src/shaders/shader.fs
@@ -5,7 +5,8 @@ in vec2 TexCoord;
 out vec4 FragColor;
 
 uniform sampler2D texture_a;
+uniform sampler2D texture_b;
 
 void main() {
-    FragColor = texture(texture_a, TexCoord);
+    FragColor = mix(texture(texture_a, TexCoord), texture(texture_b, TexCoord), 0.5);
 }
