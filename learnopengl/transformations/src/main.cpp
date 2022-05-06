6bf4d939a097de4c4a88ad4fe230f6722a3b3a15
Felipe Andrioli <felipeandrioli56@hotmail.com>
2022-05-06 08:56:13 -0300

    Added scale into the transformation list

--- learnopengl/transformations/src/main.cpp
+++ learnopengl/transformations/src/main.cpp
@@ -193,9 +193,10 @@ int main() {
         glBindTexture(GL_TEXTURE_2D, texture_b);
 
         glm::mat4 trans = glm::mat4(1.0f);
-        trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f)); 
+        trans = glm::scale(trans, glm::vec3(0.5f, 0.5f, 0.5f));
+        trans = glm::translate(trans, glm::vec3(1.0f, -1.0f, 0.0f)); 
         trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
-    
+
         unsigned int transformLoc = glGetUniformLocation(ShaderProgram.ID, "transform");
         glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
 
