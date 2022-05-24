54ca8077750b999dba4124dd8c856ebf968b8c74
Felipe Andrioli <felipeandrioli56@hotmail.com>
2022-05-24 10:04:44 -0300

    Removing comments from the previous chapter

--- learnopengl/camera/src/main.cpp
+++ learnopengl/camera/src/main.cpp
@@ -129,21 +129,6 @@ int main() {
     ShaderProgram.use();
     ShaderProgram.setInt("texture_b", 1);
 
-    /* initial vertices
-    GLfloat vertices[] = {
-        // positions      // texture coords
-        0.5f, 0.5f, 0.0f, 1.0f, 1.0f,         // top right
-        0.5f, -0.5f, 0.0f, 1.0f, 0.0f,        // bottom right
-        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f,       // bottom left
-        -0.5f, 0.5f, 0.0f, 0.0f, 1.0f         // top left
-    };
-
-    GLint indices[] = {
-        0, 1, 3,    // first triangle
-        3, 2, 1     // second triangle
-    };
-    */
-
     GLfloat vertices[] = {
         -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
         0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
@@ -203,11 +188,9 @@ int main() {
 
     GLuint VAO; 
     GLuint VBO; 
-    //GLuint EBO;
 
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
-    //glGenBuffers(1, &EBO);
 
     glBindVertexArray(VAO);
 
@@ -219,9 +202,6 @@ int main() {
 
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
-
-    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    //glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
@@ -242,63 +222,21 @@ int main() {
 
         ShaderProgram.use(); 
         
-        //glm::mat4 model = glm::mat4(1.0f);
-        //model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-        //model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
-
         glm::mat4 view = glm::mat4(1.0f);
         view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
 
         glm::mat4 projection = glm::mat4(1.0f);
         projection = glm::perspective(glm::radians(45.0f), (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 100.0f); 
 
-        // Examples of alternative ways
-
-        /*
-        unsigned int modelLoc = glGetUniformLocation(ShaderProgram.ID, "model");
-        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-        */
-
-        /*
-        unsigned int modelLoc = glGetUniformLocation(ShaderProgram.ID, "model");
-        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, &model[0][0]);
-
-        unsigned int viewLoc = glGetUniformLocation(ShaderProgram.ID, "view");
-        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]);
-
-        unsigned int projectionLoc = glGetUniformLocation(ShaderProgram.ID, "projection");
-        glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, &projection[0][0]);
-        */
-
-        /*
-        int modelLoc = glGetUniformLocation(ShaderProgram.ID, "model");
-        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-
-        int viewLoc = glGetUniformLocation(ShaderProgram.ID, "view");
-        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-
-        int projectionLoc = glGetUniformLocation(ShaderProgram.ID, "projection");
-        glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
-        */
- 
-        //ShaderProgram.setMat4("model", model);
         ShaderProgram.setMat4("view", view);
         ShaderProgram.setMat4("projection", projection);
        
         glBindVertexArray(VAO);
-        //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
      
         for (unsigned int i = 0; i < 10; i++) {
             glm::mat4 model = glm::mat4(1.0f);
             model = glm::translate(model, cubePositions[i]);
             float angle = 20.0f * (i + 1.0f) * glfwGetTime();
-
-            /*
-            if (i % 3 == 0) {
-                angle = glfwGetTime() * 25.0f;
-            } 
-            */
-
             model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
             ShaderProgram.setMat4("model", model);
             
@@ -311,7 +249,6 @@ int main() {
 
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
-    //glDeleteBuffers(1, &EBO);
     ShaderProgram.end();
 
     glfwTerminate();
