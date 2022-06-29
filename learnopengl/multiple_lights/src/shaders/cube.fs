98a9bc9bfaab3c639be6871fb591e68653577761
Felipe Andrioli <felipeandrioli56@hotmail.com>
2022-06-28 21:46:17 -0300

    Added Spotlight(flashlight) calculation

--- learnopengl/multiple_lights/src/shaders/cube.fs
+++ learnopengl/multiple_lights/src/shaders/cube.fs
@@ -47,12 +47,29 @@ struct PointLight {
 	vec3 specular;
 };
 
+struct SpotLight {
+	vec3 position;
+	vec3 direction;
+	
+	float constant;
+	float linear;
+	float quadratic;
+
+	vec3 ambient;
+	vec3 diffuse;
+	vec3 specular;
+
+	float cutOff;
+	float outerCutOff;
+};
+
 #define NR_POINT_LIGHTS 4
 
 uniform Material material;
 uniform Light light;
 uniform DirLight dirLight;
 uniform PointLight pointLights[NR_POINT_LIGHTS];
+uniform SpotLight spotLight;
 
 uniform vec3 viewPos;
 
@@ -107,6 +124,32 @@ vec3 CalcPointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir) {
 	return (ambient + diffuse + specular);	
 }
 
+vec3 CalcSpotLight(SpotLight light, vec3 normal, vec3 fragPos, vec3 viewDir) {
+	vec3 lightDir = normalize(light.position - fragPos);
+
+	// intensity
+	float theta = dot(lightDir, normalize(-light.direction));
+	float epsilon = light.cutOff - light.outerCutOff;
+	float intensity = clamp((theta - light.outerCutOff) / epsilon, 0.0, 1.0);
+
+	float diff = max(dot(normal, lightDir), 0.0);	
+	
+	vec3 reflectDir = reflect(-lightDir, normal);
+	float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+	
+	// combine results
+	vec3 ambient = light.ambient * vec3(texture(material.diffuse, TexCoords));
+	vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse, TexCoords));
+	vec3 specular = light.specular * spec * vec3(texture(material.specular, TexCoords));
+
+	ambient *= intensity;
+	diffuse *= intensity;
+	specular *= intensity;
+
+	return (ambient + diffuse + specular);
+	
+}
+
 void main() {
 
 	// properties
@@ -122,7 +165,7 @@ void main() {
 	}
 
 	// phase 3: Spot light
-	// result += CalcSpotLight(spotlight, norm, FragPos, viewDir);
+	result += CalcSpotLight(spotLight, norm, FragPos, viewDir);
 
 	FragColor = vec4(result, 1.0);
 }
