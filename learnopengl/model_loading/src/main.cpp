f1dee89587b985a7b5ee708d1094eebe3d52634e
Felipe Andrioli <felipeandrioli56@hotmail.com>
2022-07-06 21:28:31 -0300

    Added configurations and callback functions

--- learnopengl/model_loading/src/main.cpp
+++ learnopengl/model_loading/src/main.cpp
@@ -13,8 +13,23 @@
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 600
 
-void framebuffer_size_callback(int width, int height) {
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
     glViewport(width, height, 0, 0);
+    std::cout << "Viewport update -> width: " << width << " height: " << height << std::endl;
+}
+
+void processInput(GLFWwindow *window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
+void cursor_pos_callback(GLFWwindow *window, double xpos, double ypos) {
+
+}
+
+void scroll_callback(GLFWwindow *window, double xoffset, double yoffset) {
+
 }
 
 int main(int argc, char* argv[]) {
@@ -37,6 +52,12 @@ int main(int argc, char* argv[]) {
     }
 
     glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetCursorPosCallback(window, cursor_pos_callback);
+    glfwSetScrollCallback(window, scroll_callback);
+
+    // tell glfw to capture our mouse
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
 	// glad: load all OpenGL function pointers
 	// --------------------- 
@@ -46,11 +67,16 @@ int main(int argc, char* argv[]) {
         return -1;
     }
 
+    // configure global opengl state 
+    glEnable(GL_DEPTH_TEST);
+
     // application main loop
     while (!glfwWindowShouldClose(window)) {
         //render
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        processInput(window);
 
 		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved, etc)
         glfwSwapBuffers(window);
