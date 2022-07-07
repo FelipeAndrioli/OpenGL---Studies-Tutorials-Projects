78bd8419993e606f52b56471ec49aa6054cf10ef
Felipe Andrioli <felipeandrioli56@hotmail.com>
2022-07-06 22:09:10 -0300

    Added and configured vertex array and vertex buffer

--- learnopengl/model_loading/src/main.cpp
+++ learnopengl/model_loading/src/main.cpp
@@ -9,6 +9,8 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+#include "temporary_vertices.h"
+
 // settings
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 600
@@ -69,6 +71,22 @@ int main(int argc, char* argv[]) {
 
     // configure global opengl state 
     glEnable(GL_DEPTH_TEST);
+
+    unsigned int VAO;
+    unsigned int VBO;
+
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
 
     // application main loop
     while (!glfwWindowShouldClose(window)) {
