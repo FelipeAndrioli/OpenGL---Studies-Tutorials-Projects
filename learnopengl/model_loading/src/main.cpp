4154fa4856e14ad18a0ee2779bbabcb40132d53e
Felipe Andrioli <felipeandrioli56@hotmail.com>
2022-07-12 11:30:57 -0300

    Added textures configurations, load and usage

--- learnopengl/model_loading/src/main.cpp
+++ learnopengl/model_loading/src/main.cpp
@@ -12,12 +12,73 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-//#include "temporary_vertices.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#include "temporary_vertices.h"
 #include "shader.h"
 
 // settings
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 600
+
+/*
+ * TODO's
+ *
+ *  - Texture
+ *      - Class?
+ *      - Texture Map
+ *  - Camera
+ *      - Class
+ *      - callbacks
+ *  - Lighting
+ *      - Material light properties
+ *      - Light source light properties
+ *  - Types of light
+ *
+ * */
+
+unsigned int loadTexture(const char *path) {
+    unsigned int textureID;
+
+    glGenTextures(1, &textureID);
+    glBindTexture(GL_TEXTURE_2D, textureID);
+
+    int width;
+    int height;
+    int nrChannels;
+
+    unsigned char *texture_data = stbi_load(path, &width, &height, &nrChannels, 0);
+
+    if (texture_data) {
+        GLenum format;
+
+        if (nrChannels == 1) {
+            format = GL_RED;
+        } else if (nrChannels == 3) {
+            format = GL_RGB;
+        } else if (nrChannels == 4) {
+            format = GL_RGBA;
+        }
+
+        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, texture_data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+
+        // Texture wrapping/filtering options
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        
+        stbi_image_free(texture_data);
+        glBindTexture(GL_TEXTURE_2D, 0);
+ 
+    } else {
+        std::cout << "Failed to load texture at path: " << path << std::endl;
+        stbi_image_free(texture_data);
+    }
+
+    return textureID;
+}
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
     glViewport(0, 0, width, height);
@@ -73,15 +134,12 @@ int main(int argc, char* argv[]) {
         return -1;
     }
 
-    float vertices[] = {
-        // positions        
-         0.5f, -0.5f, 0.0f, // bottom right
-        -0.5f, -0.5f, 0.0f, // bottom left
-         0.0f,  0.5f, 0.0f  // top 
-    };
-
     // configure global opengl state 
     //glEnable(GL_DEPTH_TEST);
+
+    const char *texture_path = "C:/Users/Felipe/Documents/current_projects/OpenGL/learnopengl/model_loading/src/container2.png";
+   
+    unsigned int model_texture = loadTexture(texture_path); 
 
     unsigned int VAO;
     unsigned int VBO;
@@ -93,9 +151,15 @@ int main(int argc, char* argv[]) {
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
 
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+    
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 
@@ -104,7 +168,9 @@ int main(int argc, char* argv[]) {
     const char *fragment_shader_path = "C:/Users/Felipe/Documents/current_projects/OpenGL/learnopengl/model_loading/src/shaders/shader.fs";
 
     Shader ModelShader = Shader(vertex_shader_path, fragment_shader_path, nullptr);
-
+    ModelShader.use();
+    ModelShader.setInt("m_texture", 0);
+    
     // imgui initialization
     // --------------------
     IMGUI_CHECKVERSION();
@@ -115,14 +181,14 @@ int main(int argc, char* argv[]) {
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init("#version 330");
 
-    float triangle_r = 0.0f;
-    float triangle_g = 0.0f;
-    float triangle_b = 0.0f;
+    float model_r = 0.0f;
+    float model_g = 0.0f;
+    float model_b = 0.0f;
 
     glm::vec4 colors_vec;
-    colors_vec.x = triangle_r;
-    colors_vec.y = triangle_g;
-    colors_vec.z = triangle_b;
+    colors_vec.x = model_r;
+    colors_vec.y = model_g;
+    colors_vec.z = model_b;
 
     // application main loop
     while (!glfwWindowShouldClose(window)) {
@@ -136,22 +202,24 @@ int main(int argc, char* argv[]) {
 
         processInput(window);
 
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, model_texture);
         ModelShader.use();
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
 
         ImGui::Begin("Configurations");
-        ImGui::SliderFloat("Triangle R", &triangle_r, 0.0f, 1.0f); 
-        ImGui::SliderFloat("Triangle G", &triangle_g, 0.0f, 1.0f); 
-        ImGui::SliderFloat("Triangle B", &triangle_b, 0.0f, 1.0f); 
+        ImGui::SliderFloat("Model R", &model_r, 0.0f, 1.0f); 
+        ImGui::SliderFloat("Model G", &model_g, 0.0f, 1.0f); 
+        ImGui::SliderFloat("Model B", &model_b, 0.0f, 1.0f); 
         ImGui::End();
 
         ImGui::Render();
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
-        colors_vec.x = triangle_r;
-        colors_vec.y = triangle_g;
-        colors_vec.z = triangle_b;
+        colors_vec.x = model_r;
+        colors_vec.y = model_g;
+        colors_vec.z = model_b;
         ModelShader.setVec3("colors", colors_vec);
 
 		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved, etc)
