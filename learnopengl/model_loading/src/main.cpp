1912ba386bda70a5e69347abee9fa80ec86456f5
Felipe Andrioli <felipeandrioli56@hotmail.com>
2022-07-12 11:52:36 -0300

    Changing the texture to be used as the created class

--- learnopengl/model_loading/src/main.cpp
+++ learnopengl/model_loading/src/main.cpp
@@ -12,10 +12,9 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
 #include "temporary_vertices.h"
 #include "shader.h"
+#include "texture.h"
 
 // settings
 #define SCREEN_WIDTH 800
@@ -25,6 +24,7 @@
  * TODO's
  *
  *  - Texture
+ *      - Simple texture
  *      - Class?
  *      - Texture Map
  *  - Camera
@@ -35,50 +35,8 @@
  *      - Light source light properties
  *  - Types of light
  *
- * */
+ */
 
-unsigned int loadTexture(const char *path) {
-    unsigned int textureID;
-
-    glGenTextures(1, &textureID);
-    glBindTexture(GL_TEXTURE_2D, textureID);
-
-    int width;
-    int height;
-    int nrChannels;
-
-    unsigned char *texture_data = stbi_load(path, &width, &height, &nrChannels, 0);
-
-    if (texture_data) {
-        GLenum format;
-
-        if (nrChannels == 1) {
-            format = GL_RED;
-        } else if (nrChannels == 3) {
-            format = GL_RGB;
-        } else if (nrChannels == 4) {
-            format = GL_RGBA;
-        }
-
-        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, texture_data);
-        glGenerateMipmap(GL_TEXTURE_2D);
-
-        // Texture wrapping/filtering options
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        
-        stbi_image_free(texture_data);
-        glBindTexture(GL_TEXTURE_2D, 0);
- 
-    } else {
-        std::cout << "Failed to load texture at path: " << path << std::endl;
-        stbi_image_free(texture_data);
-    }
-
-    return textureID;
-}
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
     glViewport(0, 0, width, height);
@@ -139,7 +97,7 @@ int main(int argc, char* argv[]) {
 
     const char *texture_path = "C:/Users/Felipe/Documents/current_projects/OpenGL/learnopengl/model_loading/src/container2.png";
    
-    unsigned int model_texture = loadTexture(texture_path); 
+    Texture ModelTexture = Texture(texture_path);
 
     unsigned int VAO;
     unsigned int VBO;
@@ -202,8 +160,8 @@ int main(int argc, char* argv[]) {
 
         processInput(window);
 
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, model_texture);
+        ModelTexture.active();
+        ModelTexture.bind();
         ModelShader.use();
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 36);
