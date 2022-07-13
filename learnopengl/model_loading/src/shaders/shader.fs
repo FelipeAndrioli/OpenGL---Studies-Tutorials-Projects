ac63514a2ef4c14de93b720ec18afa567c3672ef
Felipe Andrioli <felipeandrioli56@hotmail.com>
2022-07-13 08:47:44 -0300

    Added structures to receive properties

--- learnopengl/model_loading/src/shaders/shader.fs
+++ learnopengl/model_loading/src/shaders/shader.fs
@@ -4,8 +4,28 @@
     Fragment Shader -> Calculate the color of the fragment
 */
 
+struct Material {
+    float shininess;
+
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
+struct Light {
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
+uniform Material material;
+uniform Light light;
+
 uniform vec3 colors;
 uniform sampler2D m_texture;
+uniform vec3 lightColor;
+
+uniform float ambient_strength;
 
 in vec3 Normal;
 in vec2 TexCoord;
@@ -13,5 +33,10 @@ in vec2 TexCoord;
 out vec4 FragColor;
 
 void main() {
-   FragColor = texture(m_texture, TexCoord);
+
+    vec4 result = texture(m_texture, TexCoord);
+
+    result *= ambient_strength;
+
+    FragColor = result;
 }
