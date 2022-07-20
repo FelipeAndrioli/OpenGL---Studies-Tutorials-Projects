7f7b8dab40237d8a2e7dade85043f61b8a1d5628
Felipe Andrioli <felipeandrioli56@hotmail.com>
2022-07-20 11:20:04 -0300

    Small test

--- learnopengl/model_loading/src/shaders/shader.fs
+++ learnopengl/model_loading/src/shaders/shader.fs
@@ -38,17 +38,13 @@ void main() {
     vec3 ambient = light.ambient * texture(material.m_diffuse_texture, TexCoord).rgb;
 
     float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = light.diffuse * diff * texture(material.m_diffuse_texture, TexCoord).rgb;
+    vec3 diffuse = light.diffuse * (diff * texture(material.m_diffuse_texture, TexCoord).rgb);
 
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
-    vec3 specular = light.specular * spec * texture(material.m_specular_texture, TexCoord).rgb;
-
+    vec3 specular = light.specular * (spec * texture(material.m_specular_texture, TexCoord).rgb);
+    
     vec3 result = (ambient + diffuse + specular);
 
     FragColor = vec4(result, 1.0);
 
 }
-
-
-
-
