08353b3213ed2abee191fbc8607d1e69fc3d8ad9
Felipe Andrioli <felipeandrioli56@hotmail.com>
2022-07-26 10:06:38 -0300

    Finishing mesh class implementation

--- learnopengl/model_loading/src/mesh.h
+++ learnopengl/model_loading/src/mesh.h
@@ -5,15 +5,31 @@
 #include <string>
 #include <vector>
 
+#include "shader.h"
+
+#define MAX_BONE_INFLUENCE 4
+
 struct Vertex {
+    // position
     glm::vec3 Position;
+    // normal
     glm::vec3 Normal;
+    // texture coordinates
     glm::vec2 TexCoords;
+    // tangent
+    glm::vec3 Tangent;
+    // bitangent
+    glm::vec3 Bitangent;
+    // bone indexes which will influence this vertex
+    int m_BoneIDs[MAX_BONE_INFLUENCE];
+    // weights from each bone
+    float m_Weights[MAX_BONE_INFLUENCE];
 };
 
 struct Texture {
     unsigned int id;
     std::string type;
+    std::string path;
 };
 
 class Mesh {
@@ -23,6 +39,12 @@ class Mesh {
         std::vector<unsigned int> indices;
         std::vector<Texture> textures;
 
-        Mesh(vector<Vertex> vertices, vector<unsigned int> indices, 
-                vector<Texture> textures);
+        Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> 
+                indices, std::vector<Texture> textures);
+        void Draw(Shader &shader);
+    private:
+        // render data
+        unsigned int VAO, VBO, EBO;
+
+        void setupMesh();
 };
