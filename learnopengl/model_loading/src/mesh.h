06eb3f12613f453e9cd95d4e37420d5ec1691fbf
Felipe Andrioli <felipeandrioli56@hotmail.com>
2022-07-25 21:42:38 -0300

    Starting a mesh class

---
+++ learnopengl/model_loading/src/mesh.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "../dependencies/glm/glm/glm.hpp"
+
+#include <string>
+#include <vector>
+
+struct Vertex {
+    glm::vec3 Position;
+    glm::vec3 Normal;
+    glm::vec2 TexCoords;
+};
+
+struct Texture {
+    unsigned int id;
+    std::string type;
+};
+
+class Mesh {
+    public:
+        // mesh data
+        std::vector<Vertex> vertices;
+        std::vector<unsigned int> indices;
+        std::vector<Texture> textures;
+
+        Mesh(vector<Vertex> vertices, vector<unsigned int> indices, 
+                vector<Texture> textures);
+};
